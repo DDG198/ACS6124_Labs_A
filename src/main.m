5036ad41523d483b423e0d1838fba98099d8cb72
DDG198 <116456618+DDG198@users.noreply.github.com>
2024-02-12 15:34:17 +0000

    completed task 1.3

--- src/main.m
+++ src/main.m
@@ -18,3 +18,21 @@ t1_2_full_mean = mean(encoder_x);
 t1_2_first_3_mean = mean(encoder_x(1:n_measurements_setup2));
 fprintf("full_mean=%f\n",t1_2_full_mean);
 fprintf("first_3_mean=%f\n",t1_2_first_3_mean);
+
+%% Task 1.3 Compute the truncated mean
+fprintf("Task 1.3\n");
+% Assume prior: encoder_x is U(21, 30)
+% Define prior distribution
+% I assume this means throw away any data outside of this range?
+
+% Filter out values outside of 21deg to 30deg
+t1_3_encoder_x_is_in_range = (21 <= encoder_x) & (encoder_x <= 30);
+t1_3_in_range_encoder_x = encoder_x(t1_3_encoder_x_is_in_range);
+
+% Compute the truncated means
+t1_3_full_mean = mean(t1_3_in_range_encoder_x);
+t1_3_first_3_mean = mean(t1_3_in_range_encoder_x(1:n_measurements_setup2));
+
+% Print results
+fprintf("full_mean=%f\n",t1_3_full_mean);
+fprintf("first_3_mean=%f\n",t1_3_first_3_mean);
