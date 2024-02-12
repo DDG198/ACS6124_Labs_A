bce8f60b4e7fd3af2a3982552a94e3697d72e86e
DDG198 <116456618+DDG198@users.noreply.github.com>
2024-02-12 14:40:53 +0000

    data loads into script

--- src/main.m
+++ src/main.m
@@ -5,3 +5,8 @@ clc;
 % Define noise statistics
 noise_mean = 0;
 noise_var = 9;
+
+%% Task 1.1 Load the data
+encoder_x = load(".\files\encoder.mat").encoder;
+n_measurements_setup1=length(encoder_x); % full measurement
+n_measurements_setup2=3; % first 3 measurement
